69127d06480f8b0a97c37568ecafada809c0dc10
joaovictorino <jhvictorino@gmail.com>
2024-06-25 22:25:40 -0300

    resolvendo teste de mutação

--- core/__tests__/model/Recibo.test.ts
+++ core/__tests__/model/Recibo.test.ts
@@ -11,4 +11,11 @@ describe("Recibo", () => {
     const recibo2: Recibo = new Recibo();
     expect(recibo1.codigo).not.toBe(recibo2.codigo);
   });
+
+  test("criar recibos com código positivo", () => {
+    const recibo1: Recibo = new Recibo();
+    const recibo2: Recibo = new Recibo();
+    expect(BigInt(recibo1.codigo).valueOf()).toBeGreaterThan(0);
+    expect(BigInt(recibo2.codigo).valueOf()).toBeGreaterThan(0);
+});
 });
